90f418e4299fef4da31fec12d838787e38f20a14
km-saifullah <kmsaifullah16@gmail.com>
2024-09-21 10:11:21 +0600

    basic type practice done

--- basic/output/script.js
+++ basic/output/script.js
@@ -1,4 +1,29 @@
 "use strict";
-const country = "I Love Bangladesh";
-console.log(country);
-console.log("first");
+// let playerName;
+// let age = 35;
+// console.log(playerName);
+// age = 3;
+// playerName = "Mashrafee";
+// playerName = 35;
+// console.log(playerName);
+// console.log(age);
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+// console.log(multiply("3", 2));
+// Array
+// let fruits = ["apple", "banana", "orange"];
+// fruits.push(34);
+// let mixed = ["apple", 34, true,{}];
+// mixed.push(34);
+// mixed.push({
+//   name: "pineapple",
+// });
+// object
+// let person = {
+//   name: "Mashrafee",
+//   age: 35,
+//   isCaptain: true,
+// };
+// person.name = "Sakib";
+// person.country = "Bangladesh";
